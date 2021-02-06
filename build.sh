9978aa154fa9ca9421f7696f525eb93abc6fa2c4
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-02-06 19:46:38 +0800

    build command samples

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-cargo build --release --manifest-path tools/fd/Cargo.toml --target-dir dist
+git submodule init --update
+
+TOOL=starship
+cargo build --release --manifest-path tools/$TOOL/Cargo.toml --target-dir dist
+
+cross build --target armv7-unknown-linux-gnueabihf  --release --manifest-path tools/$TOOL/Cargo.toml --target-dir dist
